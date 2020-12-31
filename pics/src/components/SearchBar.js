1bde198f156d2801d232619ead6048d81f6afe64
xufengFlynn <zhangxufeng26@gmail.com>
2020-12-31 14:30:11 -0800

    Created event handles

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui fluid form">
+          <div className="field">
+            <input
+              type="text"
+              placeholder="Have a try !"
+              onChange={this.onInputChange}
+            />
+            <div className="ui pointing blue basic label">
+              Pics you want to search !
+            </div>
+          </div>
         </form>
       </div>
     );
