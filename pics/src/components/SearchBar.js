1ebfd00b3d3e7494b4fd5057ef1f6f6e6b017485
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-04 10:56:03 -0800

    Invoked callbacks in children

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui fluid form">
+        <form onSubmit={this.onFormSubmit} className="ui fluid form">
           <div className="field">
             <input
               type="text"
